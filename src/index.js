0678c17e4d3597b5a5cc04ca7161e737b2135c42
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-29 20:14:54 -0300

    :sparkles: Configura roteamento inicial

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
+    <Switch>
+      <Route path='/' component={App} exact />
+      <Route path='/cadastro/video' component={App} exact />
+      <Route component={() => <div>Página 404</div>} />
+    </Switch>
+  </Router>,
+  /* <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>, */
   document.getElementById('root')
 );
