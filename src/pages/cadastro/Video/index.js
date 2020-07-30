73c2c4f143fe9eb596c8da45faef2b90e738ecbe
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-29 21:05:56 -0300

    :nail_care: Estiliza PageDefault

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function CadastroVideo() {
-  return <div>Página de cadastro de vídeos</div>;
+  return (
+    <PageDefault>
+      <h1>Página de cadastro de vídeos</h1>
+
+      <Link to='/cadastro/categoria'>Cadastrar categoria</Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
